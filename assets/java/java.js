4bca67f81f1fd44f5045853e27acd1c4f6705462
Michael Astalos <michaeldont@gmail.com>
2021-07-15 22:33:00 -0400

    Quiz will work from start to finish screen as intended, next work on saving results and viewing result page

--- assets/java/java.js
+++ assets/java/java.js
@@ -8,7 +8,9 @@ var questions =[
     {question:"This is the question 6 text", choices:["Option1", "Option2", "Option3", "Option4"], answer:1},
     {question:"This is the question 7 text", choices:["Option1", "Option2", "Option3", "Option4"], answer:1},
     {question:"This is the question 8 text", choices:["Option1", "Option2", "Option3", "Option4"], answer:1},
-    {question:"This is the question 9 text", choices:["Option1", "Option2", "Option3", "Option4"], answer:1}
+    {question:"This is the question 9 text", choices:["Option1", "Option2", "Option3", "Option4"], answer:1},
+    {question:"This is the question 10 text", choices:["Option1", "Option2", "Option3", "Option4"], answer:1}
+
 ];
 
 var content = document.querySelector("#quiz-box");
@@ -27,43 +29,66 @@ function removeContent(){
 //populate quiz-box with multiple choice questions
 function generateQuestion() {
     if(iteration<questions.length){
-        var questionNumber = iteration;
-        removeContent(content);
-        
-        var questionPrompt = $('<h2>'+questions[questionNumber].question+'</h2>');
+        removeContent();
+        var questionPrompt = $('<h2>'+questions[iteration].question+'</h2>');
         $('#quiz-box').append(questionPrompt);
 
 
         var questionChoices = $('<div class="choices">');
         $("#quiz-box").append(questionChoices);
 
-        for (n=0;n<questions[questionNumber].choices.length;n++){
-            var questionBtn = $('<button class="choice-option">'+questions[questionNumber].choices[n]+'</button>');
+        for (n=0;n<questions[iteration].choices.length;n++){
+            var questionBtn = $('<button class="choice-option">'+questions[iteration].choices[n]+'</button>');
             questionBtn.attr('data-question-option',n);
             $(".choices").append(questionBtn);
         }
-        iteration++;
     }
 }
 
 //question option click event
 $("#quiz-box").on('click', '.choice-option' , function(event) {
-    event.preventDefault();
-    var selectedAnswer = $(this).attr('data-question-option');
-    var correctAnwser = questions[iteration].answer;
+    event.preventDefault();   
+    //creates containers for results
     var resultContainer = $('<div class="answer-result">')
     var result = $('<p>');
+
+    //grabs answer selected and checks to see if it's correct
+    var selectedAnswer = $(this).attr('data-question-option');
+    var correctAnwser = questions[iteration].answer;
     if (selectedAnswer == correctAnwser) {
         result.text('Correct!');
         score++;
+    } else {result.text('Wrong...');}
+
+    //checks to see if there are more questions, if not display resultPage
+    if (iteration<questions.length-1){
+        iteration++;
+        generateQuestion();
     }
-    else {
-        result.text('Wrong...');
+    else{
+        console.log("work");
+        removeContent();
+        resultsPage();
     }
-    removeContent();
-    generateQuestion();
-    $("#quiz-box").append(resultContainer.append(result));
+
+    //adds created content to form
+    $("#quiz-box").append(resultContainer.append(result));  
 });
+
+//Results page
+function resultsPage() {
+    var finalScore = Math.round((score/questions.length)*100)
+    var resultScreen = $(
+        '<h3>All Done!</h3>'+
+        '<p>Your final score is '+finalScore+'.</p>'+
+        '<label for="initials">Enter in initials to your record score:</label>'+
+        '<input type="text" name="initials" id="initials" placeholder="AAA"/>'+
+        '<button class="submit">Submit</button>'
+        );
+    $("#quiz-box").append(resultScreen);
+
+}
+
 
 //reset quiz ------------ combined with start, if not nessessary
 function reset() {
